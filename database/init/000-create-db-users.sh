321f1e7f2103f53a727143f41c9a00b6e2b1dc24
unknown <ju.velling@gmail.com>
2026-09-15 03:57:10 +0200

    added automated user creation to db setup

---
+++ database/init/000-create-db-users.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+create_role() {
+    local username="$1"
+    local secret_file="$2"
+    local password
+
+    password="$(cat "/run/secrets/$secret_file")"
+
+    psql \
+        --username "${POSTGRES_USER:-postgres}" \
+        --dbname "${POSTGRES_DB:-${POSTGRES_USER:-postgres}}" \
+        --set=app_username="$username" \
+        --set=app_password="$password" \
+        <<'EOSQL'
+CREATE ROLE :"app_username"
+    LOGIN
+    PASSWORD :'app_password';
+EOSQL
+}
+
+# create_role "backend" "pg_pw_backend"
