93b4c92db45813619956305533724f4f68d719c0
unknown <ju.velling@gmail.com>
2026-09-16 15:11:34 +0200

    added group roles to db

--- database/init/001-create-db-users.sh
+++ database/init/001-create-db-users.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 create_role() {
     local username="$1"
     local secret_file="$2"
+    local role="$3"
     local password
 
     password="$(cat "/run/secrets/$secret_file")"
@@ -13,11 +14,13 @@ create_role() {
         --dbname "${POSTGRES_DB:-${POSTGRES_USER:-postgres}}" \
         --set=app_username="$username" \
         --set=app_password="$password" \
+        --set=app_role="$role" \
         <<'EOSQL'
 CREATE ROLE :"app_username"
     LOGIN
-    PASSWORD :'app_password';
+    PASSWORD :'app_password'
+    IN ROLE :"app_role";
 EOSQL
 }
 
-create_role "backend" "pg_pw_backend"
+create_role "backend-1" "pg_pw_backend" "backend"
