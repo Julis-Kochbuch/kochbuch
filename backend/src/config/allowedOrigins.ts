b35f029a6b7a973fac9ca888d0fcca10266380df
unknown <ju.velling@gmail.com>
2026-09-16 13:21:50 +0200

    added https port

--- backend/src/config/allowedOrigins.ts
+++ backend/src/config/allowedOrigins.ts
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-    `http://${process.env.HOSTNAME!}${process.env.PROXY_PORT ? `:${process.env.PROXY_PORT}` : ``}`,
-    `https://${process.env.HOSTNAME!}${process.env.PROXY_PORT ? `:${process.env.PROXY_PORT}` : ``}`
+    `http://${process.env.HOSTNAME!}${process.env.HTTP_PROXY_PORT ? `:${process.env.HTTP_PROXY_PORT}` : ``}`,
+    `https://${process.env.HOSTNAME!}${process.env.HTTPS_PROXY_PORT ? `:${process.env.HTTPS_PROXY_PORT}` : ``}`
 ];
 
 export default allowedOrigins;
