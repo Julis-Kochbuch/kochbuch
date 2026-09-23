869d98daee9a0d5251a9643eecdbf23f624ca25c
unknown <ju.velling@gmail.com>
2026-09-24 01:21:55 +0200

    added redis session store

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,7 @@ import rateLimitConfig from './config/rateLimit.js';
 import cors from 'cors';
 import corsConfig from './config/cors.js';
 import session from 'express-session';
+import store from './config/redis.js';
 import waitPort from 'wait-port';
 import fs from "node:fs";
 
@@ -33,6 +34,7 @@ app.use(express.json());
 
 app.use(
     session({
+        store: store,
         secret: sessionSecret,
         resave: false,
         saveUninitialized: false,
